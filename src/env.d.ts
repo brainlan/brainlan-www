e6e50c57d082f3b70a15ca6f6c719c8c4e76a6f4
ohrrkan <ohrrkan@brainlan.com>
2024-02-20 18:22:21 +0100

    sitemap

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_COMPANY: string;
+  readonly PUBLIC_DEBUG: boolean;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
