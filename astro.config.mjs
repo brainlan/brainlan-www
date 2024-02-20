c091447e6ba82cd52b6afac6b58a5d2e8f1645ec
ohrrkan <ohrrkan@brainlan.com>
2024-02-20 18:00:22 +0100

    integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,26 @@
 import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
-
 let adapter = vercel({
   webAnalytics: {
     enabled: true,
     maxDuration: 60,
   },
 });
-
 let site = "https://www.brainlan.com";
-
 if (process.argv[3] === "--node") {
-  adapter = node({ mode: "standalone" });
+  adapter = node({
+    mode: "standalone",
+  });
   site = "http://localhost:4321";
 }
 
+// https://astro.build/config
 export default defineConfig({
   site,
   output: "hybrid",
   adapter,
+  integrations: [sitemap(), tailwind()],
 });
