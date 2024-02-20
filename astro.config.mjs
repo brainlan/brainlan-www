be14f8451cb4abb0e0b770cd9e7c9bbeb7a3073a
ohrrkan <ohrrkan@brainlan.com>
2024-02-20 16:53:09 +0100

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
+let adapter = vercel({
+  webAnalytics: {
+    enabled: true,
+    maxDuration: 60,
+  },
+});
+
+let site = "https://www.brainlan.com";
+
 // https://astro.build/config
 export default defineConfig({
+  site,
   output: "hybrid",
-  adapter: vercel(),
+  adapter,
 });
